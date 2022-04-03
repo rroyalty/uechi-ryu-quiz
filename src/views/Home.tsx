f5974d2352b51ca49627801d4bdb30048b060c52
rroyalty <rroyalty@gmail.com>
2022-04-03 12:26:30 -0400

    Test

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -12,14 +12,14 @@ const HomeDiv = styled.div`
             margin: 25px;
             font-family: arial;
             font-weight: 400;
-            font-size: 5rem;
+            font-size: 3rem;
             max-width: 66vw;
         };
         & > h2 {
             margin: 25px;
             font-family: calluna;
             font-weight: 300;
-            font-size: 3.5rem;
+            font-size: 2rem;
             color: red;
             max-width: 66vw;
         };
@@ -45,10 +45,11 @@ const Home: React.FC = (): JSX.Element => {
 
     return (
         <HomeDiv>
-            <h1>UECHIRYU BUTOKUKAI</h1>
+            <h1>UECHIRYU</h1> 
+            <h1>BUTOKUKAI</h1>
             <h2> History & Terminology Quiz</h2>
-            <p>The following is a timed multiple choice trivia quiz game created to help black belt candidates prepare for the written portion of the black belt test. I wanted something more fun and engaging than simple flash cards, because I've always hated those, so I decided to build a trivia game to help me study. The rules of the game are as follows:</p>
-            <ul>
+            {/* <p>The following is a timed multiple choice trivia quiz game created to help black belt candidates prepare for the written portion of the black belt test. I wanted something more fun and engaging than simple flash cards, because I've always hated those, so I decided to build a trivia game to help me study. The rules of the game are as follows:</p> */}
+            {/* <ul>
                 <li>The player will start with 60 seconds on the clock.</li>
                 <li>Correct answers will add 3 seconds of time to the clock.</li>
                 <li>Incorrect answers will remove 5 seconds of time from the clock.</li>
@@ -56,7 +57,7 @@ const Home: React.FC = (): JSX.Element => {
                 <li>Questions will be presented in a random order every playthrough.</li>
                 <li>Answers will be presented in a random order for every question.</li>
             </ul>
-            <p>Enjoy!</p>
+            <p>Enjoy!</p> */}
             <div>
                 <Link to='questions/0'>Begin</Link>
             </div>
